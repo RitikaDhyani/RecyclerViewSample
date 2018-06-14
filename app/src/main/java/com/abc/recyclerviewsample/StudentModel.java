8244498c31069ba661c552a7bf804c3ad85e13fc
Ritika Dhyani <39532567+RitikaDhyani@users.noreply.github.com>
2018-06-14 15:54:38 +0530

    Initial commit

---
+++ app/src/main/java/com/abc/recyclerviewsample/StudentModel.java
@@ -0,0 +1,10 @@
+package com.abc.recyclerviewsample;
+
+public class StudentModel {
+
+    String studentName;
+
+    public StudentModel(String studentName) {
+        this.studentName = studentName;
+    }
+}
